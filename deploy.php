bd166227596cf1dfb42a50f338aee44c3dc4cf8a
Anup Nayak <115527833+mrnayak25@users.noreply.github.com>
2022-12-19 20:57:46 +0530

    Updated

--- deploy.php
+++ deploy.php
@@ -1,5 +1,15 @@
 
 <?php
+
+**
+  * GIT DEPLOYMENT SCRIPT
+  *
+  * Used for automatically deploying websites via github or bitbucket, more deets here:
+  * https://gist.github.com/riodw/71f6e2244534deae652962b32b7454e2
+  * How To Use:
+  * https://medium.com/riow/deploy-to-production-server-with-git-using-php-ab69b13f78ad
+  */
+
 $dbHost = 'localhost';
 $dbName = 'luckytoot';
 $dbUser = 'root';
